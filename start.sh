e521846fa644c082bf0727adb3b173551e538986
dung11122005 <dungakaishi900@gmail.com>
2025-05-16 15:37:46 +0700

    1

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-php artisan storage:link
-php artisan serve --host=0.0.0.0 --port=${PORT:-8000
+
+# Đợi database sẵn sàng (tuỳ chọn)
+# Bạn có thể dùng thêm sleep nếu cần
+# sleep 10
+
+# Chạy migration và seeding nếu cần
+php artisan migrate --force
+
+# Khởi động server Laravel
+php artisan serve --host=0.0.0.0 --port=80
